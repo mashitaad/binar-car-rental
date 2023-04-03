e3d7b3c4ec8f9729479b2261d7f1befe57f39d41
mashitaad <mashitaad@gmail.com>
2023-04-03 14:01:38 +0700

    feat: constant Faq

---
+++ src/components/constant/listFaq.js
@@ -0,0 +1,34 @@
+const faqCom = [
+    {
+      id: 1,
+      question: "Apa saja syarat yang dibutuhkan?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      id: 2,
+      question: "Berapa hari minimal sewa mobil lepas kunci?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      id: 3,
+      question: "Berapa hari sebelumnya sabaiknya booking sewa mobil?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      id: 4,
+      question: "Apakah Ada biaya antar-jemput?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+      id: 5,
+      question: "Bagaimana jika terjadi kecelakaan",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  ];
+
+  export { faqCom };
