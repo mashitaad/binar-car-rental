2a97f8a8d8608f1653107b9d0b476e340628769c
mashitaad <mashitaad@gmail.com>
2023-04-03 14:14:29 +0700

    feat: add landingPage pages

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Hero from '../components/homepage/hero/Hero'
+import OurService from '../components/homepage/ourService/Service'
+import WhyUs from '../components/homepage/whyus/WhyUs'
+import Testimonial from '../components/homepage/testimonial/Testimonial'
+import CtaSection from '../components/homepage/cta/cta'
+import Faq from '../components/homepage/faq/Faq'
+import Navbar from "../components/header/navbar/Navbar"
+import Footer from "../components/header/footer/Footer"
+import { serviceCom } from '../components/constant/listService'
+import { whyUsCom } from '../components/constant/listwhyUs'
+import { testimonialCom } from '../components/constant/lisTestimonial'
+import { faqCom } from '../components/constant/listFaq'
+import { answerCom } from '../components/constant/listAnswer'
+const props = {
+  serviceCom, 
+  whyUsCom, 
+  testimonialCom, 
+  faqCom, 
+  answerCom
+}
+
+
+export default function HomePage() {
+  return (
+    <>
+    <Navbar/>
+    <main>
+      <Hero />
+      <OurService {...props}/>
+      <WhyUs {...props}/>
+      <Testimonial {...props} />
+      <CtaSection />
+      <Faq {...props}/>
+    </main>
+    <Footer />
+    </>
+  )
+}
