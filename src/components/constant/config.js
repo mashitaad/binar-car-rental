2e3f5c5a7bebf01457c5fbea83a2821f00f71ae7
mashitaad <mashitaad@gmail.com>
2023-04-04 01:47:08 +0700

    feat: add env

---
+++ src/components/constant/config.js
@@ -0,0 +1,9 @@
+const config = {
+    APP_NAME: process.env.REACT_APP_NAME,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+    API_BASE_PATH: process.env.REACT_APP_API_BASE_PATH,
+    API_URL:
+      process.env.REACT_APP_API_BASE_URL + process.env.REACT_APP_API_BASE_PATH,
+  }
+  
+  export default config
