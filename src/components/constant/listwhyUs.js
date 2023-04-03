26f397316ad87aebb4c75459bc059a1d06c13805
mashitaad <mashitaad@gmail.com>
2023-04-03 14:02:57 +0700

    feat: constant whyus

---
+++ src/components/constant/listwhyUs.js
@@ -0,0 +1,33 @@
+import icon1 from "../../assets/icon/whyus_icon_1.svg";
+import icon2 from "../../assets/icon/whyus_icon_2.svg";
+import icon3 from "../../assets/icon/whyus_icon_3.svg";
+import icon4 from "../../assets/icon/whyus_icon_4.svg";
+
+const whyUsCom = [
+    {
+      id: 1,
+      icon: icon1,
+      title: "Mobil Lengkap",
+      desc: "Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat",
+    },
+    {
+      id: 2,
+      icon: icon2,
+      title: "Harga Murah",
+      desc: "Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain",
+    },
+    {
+      id: 3,
+      icon: icon3,
+      title: "Layanan 24 Jam",
+      desc: "Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu",
+    },
+    {
+      id: 4,
+      icon: icon4,
+      title: "Sopir Profesional",
+      desc: "Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu",
+    },
+  ];
+
+  export { whyUsCom };
