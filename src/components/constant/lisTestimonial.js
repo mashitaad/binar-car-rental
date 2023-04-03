47a938f82c4cb4fbf19ca531c76df50dfcc945a1
mashitaad <mashitaad@gmail.com>
2023-04-03 14:00:49 +0700

    feat: constant testimonial

---
+++ src/components/constant/lisTestimonial.js
@@ -0,0 +1,68 @@
+import person_1 from "../../assets/img/img_photo_1.png";
+import person_2 from "../../assets/img/img_photo_2.png";
+
+const testimonialCom = [
+
+    {
+      id: 1,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_1,
+      star: 5,
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_2,
+      star: 5,
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_1,
+      star: 5,
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_2,
+      star: 5,
+    },
+    {
+      id: 5,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_1,
+      star: 5,
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      age: 32,
+      city: "Bromo",
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic distinctio ratione cumque placeat, mollitia culpa? Corporis repellendus saepe ipsam maiores odit inventore quo deleniti amet!",
+      image: person_2,
+      star: 5,
+    },
+  ];
+
+  export { testimonialCom };
