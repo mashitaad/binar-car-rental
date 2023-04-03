50db974f996be4b2bc5b1c29e3c9b12cd7d03439
mashitaad <mashitaad@gmail.com>
2023-04-03 14:02:24 +0700

    feat: constant service

---
+++ src/components/constant/listService.js
@@ -0,0 +1,31 @@
+import icon_check from "../../assets/icon/service_check.svg";
+
+const serviceCom = [
+    {
+      id: 1,
+      image: icon_check,
+      content: "Sewa Mobil Dengan Supir di Bali 12 Jam",
+    },
+    {
+      id: 2,
+      image: icon_check,
+      content: "Sewa Mobil Lepas Kunci di Bali 24 Jam",
+    },
+    {
+      id: 3,
+      image: icon_check,
+      content: "Sewa Mobil Jangka Panjang Bulanan",
+    },
+    {
+      id: 4,
+      image: icon_check,
+      content: "Gratis Antar - Jemput Mobil di Bandara",
+    },
+    {
+      id: 5,
+      image: icon_check,
+      content: "Layanan Airport Transfer / Drop In Out",
+    },
+  ];
+
+  export { serviceCom };
